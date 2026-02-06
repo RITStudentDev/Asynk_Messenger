d4d05e16d2fa695ce91515a5d2ec89da132bf73b
Jacob Bolton <jacobbolton@Jacobs-MacBook-Pro-2.local>
2026-02-05 23:55:57 -0500

    added contact generation

--- back/User/models.py
+++ back/User/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class User(models.Model):
-    userId = models.CharField(max_length=255, unique=True, primary_key=True)
+    userId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    contact = models.CharField(max_length=20, blank=True, null=True)
+    contact = models.CharField(max_length=9, blank=True, unique=True)
     createdAt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- back/User/serializers.py
+++ back/User/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import User
+import string
+from django.utils.crypto import get_random_string
 
 # General serializer for user data
 class UserSerializer(serializers.ModelSerializer):
@@ -29,12 +31,25 @@ class SignupSerializer(serializers.ModelSerializer):
             if value['password'] != value['password2']:
                 raise serializers.ValidationError({'password2': 'Passwords do not match'})
             return value
-        
+
+        # Generates 9 digit contact
+        def generate_contact(self):
+            if User.objects.count < 900000000:
+                while True:
+                    contact = get_random_string(9, allowed_chars=string.digits)
+                    if not User.objects.filter(unique_id=contact).exists():
+                        return contact
+            raise Exception('Max user count reached')
+
         # Add new user to database
         def create(self, validated_data):
             validated_data.pop('password2')
             password = validated_data.pop('password')
             user = User(**validated_data)
             # create function to add the password
+
+            if not self.contact:
+                self.contact = self.generate_contact()
+
             user.save()
             return user
