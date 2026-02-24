1fe8a7b5a3db1a74e4d7a220b67f2204626310fc
Jacob Bolton <jacobbolton@Jacobs-MacBook-Pro-2.local>
2026-02-24 15:59:27 -0500

    added basic components for user hub page

--- front/src/pages/HubPage.jsx
+++ front/src/pages/HubPage.jsx
@@ -1,11 +1,11 @@
-
+import HubSideBar from "../components/HubSideBar"
 import RoomProfile from "../components/RoomProfile"
 
 function HubPage (){
 
     return(
         <>
-            <RoomProfile/>
+            <HubSideBar/>
         </>
     )
 }
