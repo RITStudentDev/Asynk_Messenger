6eebe5219f4ca9e6cfce52b513eaea2a61621536
Jacob Bolton <jabolton939@gmail.com>
2026-03-05 16:21:25 -0500

    stored base url in variable

--- front/src/mod/user.js
+++ front/src/mod/user.js
@@ -1,10 +1,10 @@
 export async function login (username, password){
 
-    //const BASE_URL = 'http://localhost:8000/' 
+    const BASE_URL = "http://localhost:8000/" 
     // CXf25nXw
 
     try {
-        const response = await fetch( "http://localhost:8000/api/token/", {
+        const response = await fetch( `${BASE_URL}api/token/`, {
             method: "POST",
             headers: {
                 "Content-Type" : "application/json",
@@ -27,4 +27,8 @@ export async function login (username, password){
     } catch (error){
         throw error
     }
+}
+
+export async function get_logged_user(){
+
 }
