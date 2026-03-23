29369bae9cd765818d51fa6ca74b3bb8d4ad7041
Jacob Bolton <jabolton939@gmail.com>
2026-03-23 11:46:43 -0400

    switched to docker contained db

--- back/back/settings.py
+++ back/back/settings.py
@@ -110,9 +110,9 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DBNAME'),
-        'USER': os.getenv('DBUSER'),
-        'PASSWORD': os.getenv('DBPASSWORD'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('DBHOST'),
         'PORT': os.getenv('DBPORT'),
     }
